72751413d53245074c26ac9d54907520e4e475c0
Tom Trevethan <tptrevethan@googlemail.com>
2018-10-12 17:31:21 +0100

    added s3 upload to object init

--- object_init.py
+++ object_init.py
@@ -2,13 +2,18 @@
 
 import amap.mapping as am
 import bitcoin as bc
+import boto3
 
-print("Initialise the mapping object and add assets")
+print("Initialise the mapping object")
 print("Set 2-of-3 multisig policy")
 
 map_obj = am.MapDB(2,3)
+map_obj.update_time()
 
 print(" ")
 
 print("Export object")
 map_obj.export_json('map.json')
+print("Upload to S3")
+s3 = boto3.resource('s3')
+s3.Object('cb-mapping','map.json').put(Body=open('map.json','rb'))
