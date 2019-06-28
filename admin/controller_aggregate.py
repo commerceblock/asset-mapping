5db4b8bd6a944c47512becb246dd5f0a1150b8cb
Tom Trevethan <tptrevethan@googlemail.com>
2019-06-28 15:39:27 +0100

    controller aggregate include scriptpubkey

--- admin/controller_aggregate.py
+++ admin/controller_aggregate.py
@@ -12,7 +12,7 @@ controller_pub_keys = []
 
 c1_public = input("Enter the public key of controller 1: ")
 if len(c1_public) != 66:
-    print("ERROR: public key format error - must be conpressed")
+    print("ERROR: public key format error - must be compressed")
     print("Exit")
     sys.exit()
 
@@ -21,7 +21,7 @@ print(" ")
 
 c2_public = input("Enter the public key of controller 2: ")
 if len(c2_public) != 66:
-    print("ERROR: public key format error - must be conpressed")
+    print("ERROR: public key format error - must be compressed")
     print("Exit")
     sys.exit()
 
@@ -30,7 +30,7 @@ print(" ")
 
 c3_public = input("Enter the public key of controller 3: ")
 if len(c3_public) != 66:
-    print("ERROR: public key format error - must be conpressed")
+    print("ERROR: public key format error - must be compressed")
     print("Exit")
     sys.exit()
     
@@ -60,11 +60,18 @@ print("Creating 2-of-3 multisig address from the controller public keys")
 result = ocean.call('createmultisig',2,controller_pub_keys)
 print(" ")
 
+validate = ocean.call('validateaddress',result['address'])
+result['scriptPubKey'] = validate['scriptPubKey']
+
 print("P2SH address: "+result['address'])
 print(" ")
 print("Redeem script: "+result['redeemScript'])
 print(" ")
+print("scriptPubKey: "+result['scriptPubKey'])
+print(" ")
 
+print("CHECK: address encoding and prefix matches network settings")
+print(" ")
 print("Exporting P2SH data to p2sh.json")
 with open('p2sh.json','w') as file:
     json.dump(result,file)
